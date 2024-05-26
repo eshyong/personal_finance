e8d157627c2bee4e5a40b93d4f724e5319557664
Eric Shyong <eric.shyong@gmail.com>
2024-05-25 20:50:15 -0700

    send emails via gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "bear-finance-f6d6f261d92f.herokuapp.com", port: 443 }
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Send emails via gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "bear-finance-f6d6f261d92f.herokuapp.com" }
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            "eshyong90",
+    password:             Rails.application.credentials.gmail_app_password!,
+    authentication:       "plain",
+    enable_starttls_auto: true,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

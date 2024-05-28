aa78ff9c030f783899e160f2492959b547670006
Eric Shyong <eric.shyong@gmail.com>
2024-05-28 12:17:25 -0700

    use number_to_currency helper

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,8 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :financial_account
 
   def get_amount_in_dollars
-    amt = amount.nil? ? 0.0 : amount / 100.0
-    format("%.2f", amt)
+    amount.nil? ? 0.0 : amount / 100.0
   end
 
   def format_transacted_at
